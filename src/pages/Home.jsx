8787c642921e04106f7ac43f484e4ab9b65aa4ae
AndreyZhitenev <zhitenev.andr@gmail.com>
2022-07-05 14:57:06 +0300

    upgraded navigation with react router, added single page for every item

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import qs from 'qs';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
@@ -85,7 +85,11 @@ const Home = () => {
 		isMounted.current = true;
 	}, []);
 
-	const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+	const pizzas = items.map((obj) => (
+		<Link key={obj.id} to={`/pizza/${obj.id}`}>
+			<PizzaBlock {...obj} />
+		</Link>
+	));
 	const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
 
 	return (
